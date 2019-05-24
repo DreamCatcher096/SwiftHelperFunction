a50d37308092047a33792136fdfa52b80f920581
DreamCatcher096 <610624885@qq.com>
2019-05-24 16:20:11 +0800

    Add some commit

--- io_function.swift
+++ io_function.swift
@@ -9,6 +9,10 @@
 import Foundation
 import CoreML
 
+/// Read binary file (e.g. .bin files) to MLMultiArray as Float
+/// - parameter path: Path to the binary file.
+/// - parameter target: The target MLMultiArray object.
+/// - parameter length: Length of binary file
 func FileToMLMultiArrayFloat(path: String, target: MLMultiArray, length: Int)
 {
     let input_data = NSData(contentsOfFile: path)
